8a506d4f4ed6f48d0d9c4e48765eeb3a933c5fab
cstlford <cstlford@gmail.com>
2025-04-25 13:38:48 -0500

    rm config

--- public/scripts/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  apiBaseUrl:
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
-      ? "http://localhost:5750"
-      : "https://your-api-domain.herokuapp.com",
-};
-
-export default config;
